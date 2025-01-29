b6c5b4db462d232e63114bd778749e0b335e9426
Koushik Paul <72144417+Koushik1687@users.noreply.github.com>
2025-01-29 14:19:45 +0530

    Add files via upload

---
+++ Python/Assignment2.9.py
@@ -0,0 +1,8 @@
+import cmath
+a = int(input("Please enter a :"))
+b = int(input("Please enter b :"))
+c = int(input("Please enter c :"))
+u = pow(b, 2)
+y = cmath.sqrt(u - 4(a*c))
+x = -b + y/2*a
+print(x)
