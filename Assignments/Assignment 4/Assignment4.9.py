df7b162304bac6d781fe3f6d339eaccf34fea14d
Koushik Paul <koushikp72@gmail.com>
2025-02-23 11:51:03 +0530

    Update Assignment4.9.py

--- Assignments/Assignment 4/Assignment4.9.py
+++ Assignments/Assignment 4/Assignment4.9.py
@@ -1,15 +1,13 @@
 lt = [0,0,0,0,0,0]
-l = ["Bengali","English","Mathematis","Science","History","Geography"]
+l = ["Bengali","English","Mathematics","Science","History","Geography"]
 for i in range(0,6):
-    print("Please enter",l[i],"'s Makrs :")
+    print("Please enter",l[i],"'s Marks :")
     lt[i] = int(input())
-sum = 0
 print("\nThe marks you got: ")
-for i in range(0,6):
+for i in range(6):
     print(l[i],"Makrs :",lt[i])
-    sum +=lt[i]
-print()
+
+print()  
 print("The Maximum makrs :",max(lt))
 print("The Minimum makrs :",min(lt))
-
-print("The total Marks obtain is :",sum)
+print("The total Marks obtained is :",sum(lt))
